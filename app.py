6db4b4afaddd61c27f430bef680d961666abe94a
Patrick Sontag <pmsontag84@gmail.com>
2022-06-24 09:54:18 -0500

    added JS and CSS

--- app.py
+++ app.py
@@ -1,7 +1,11 @@
 from boggle import Boggle
-from flask import Flask
+from flask import Flask, request, render_template, jsonify, session
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = "kjsdf"
 
 boggle_game = Boggle()
+
+@app.route("/")
+def home():
+    return render_template("home.html")
