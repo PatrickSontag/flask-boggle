03597c531b48c4b98e5676a2ccb10c26deb05723
Patrick Sontag <pmsontag84@gmail.com>
2022-06-27 18:26:16 -0500

    home and new game html added

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from boggle import Boggle
-from flask import Flask, request, render_template, jsonify, session
+from flask import Flask, request, render_template, jsonify, session, redirect, flash
+# from flask_debugtoolbar import DebugToolbarExtension
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = "kjsdf"
@@ -8,10 +9,28 @@ boggle_game = Boggle()
 
 @app.route("/")
 def home():
+    """Generate and show form to start game"""
 
     board = boggle_game.make_board()
-    # session['board'] = board
+    session['board'] = board
     # print("board: ", board)
     # print("session: ", session)
+    print("home")
 
     return render_template("home.html", board=board)
+
+@app.route("/begin", methods=["POST"])
+def begin():
+    """Redirect to new game"""
+
+    session['guesses'] = []
+    print("begin")
+
+    return redirect ("/new_game")
+
+@app.route("/new_game")
+def new_game():
+    """Show new game board"""
+    
+    print("new game")
+    return render_template("new_game.html")
